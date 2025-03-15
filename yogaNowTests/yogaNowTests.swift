1f359d2058d43f65c5439fe37178a86774496163
Pjh <angelaiz@naver.com>
2025-03-15 21:14:03 +0900

    Initial Commit

---
+++ yogaNowTests/yogaNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  yogaNowTests.swift
+//  yogaNowTests
+//
+//  Created by somefit on 3/15/25.
+//
+
+import Testing
+@testable import yogaNow
+
+struct yogaNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
